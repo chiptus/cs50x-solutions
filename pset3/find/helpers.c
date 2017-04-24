613637017cc860a493df094dcc52659dc3e26713
Chaim Lando <ubuntu@dfc2eabdf236>
2017-04-24 13:14:30 +0000

    tests + naive

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -8,20 +8,39 @@
 
 #include "helpers.h"
 
+void replace(int values[], int i, int j);
+
 /**
  * Returns true if value is in array of n values, else false.
  */
+ //TODO implementation is O(n)
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
-    return false;
+    bool found = false;
+    for (int i=0; i<n && !found; i++) {
+        found = values[i] == value;
+    }
+    return found;
 }
 
 /**
- * Sorts array of n values.
+ * Sorts array of n values from small to big.
  */
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    for (int i=0; i<n; i++) {
+        for (int j=i+1; j < n; j++) {
+            if (values[i] > values[j]) {
+                replace(values, i, j);
+            }
+        }
+    }
     return;
+}
+
+void replace(int values[], int i, int j) {
+    int temp = values[i];
+    values[i] = values[j];
+    values[j] = temp;
 }
--- pset3/find/test.c
+++ pset3/find/test.c
@@ -12,10 +12,16 @@ void return_true_if_val_is_in_values(int values[]);
 void return_false_if_val_is_not_in_values(int values[]);
 void sort_from_small_to_large(int values[]);
 
+bool small_to_large(int values[], int len);
+void test_small_to_large();
+void return_true_if_values_is_sorted_from_small_to_large();
+void return_false_if_values_is_not_sorted_from_small_to_large();
+
 int main(void) {
     int values[10] = {36623, 9397, 47119, 41858, 5706, 24921, 64447, 60790, 48236, 7389};
     test_search(values);
     test_sort(values);
+    test_small_to_large();
 }
 
 void test_search(int values[]){
@@ -32,6 +38,7 @@ void return_false_if_n_is_negative(int values[]) {
     printf("\nSearch() should return false when n is negative: ");
     if (search(10, values, -10)) {
         printf("FAIL\n");
+        return;
     }
     printf("SUCCESS\n");
 }
@@ -40,6 +47,7 @@ void return_true_if_val_is_in_values(int values[]){
     printf("\nSearch() should return true when val is in values: ");
     if (search(9397, values, 10)) {
         printf("SUCCESS\n");
+        return;
     }
     printf("FAIL\n");
 }
@@ -48,10 +56,48 @@ void return_false_if_val_is_not_in_values(int values[]){
     printf("\nSearch() should return false when val is not in values: ");
     if (search(10, values, 10)) {
         printf("FAIL\n");
+        return;
     }
     printf("SUCCESS\n");
 }
 
-void sort_from_small_to_large() {
-    
+void sort_from_small_to_large(int values[]) {
+    printf("\nsort() should sort values from small to large: ");
+    sort(values, 10);
+    if (small_to_large(values, 10)) {
+        printf("SUCCESS\n");
+        return;
+    }
+    printf("FAIL\n");
+}
+
+bool small_to_large(int values[], int len) {
+    bool flag = true;
+    for (int i=1; i < len && flag; i++) {
+        flag = values[i-1] < values[i];
+    }
+    return flag;
+}
+
+void test_small_to_large(){
+    return_true_if_values_is_sorted_from_small_to_large();
+    return_false_if_values_is_not_sorted_from_small_to_large();
+}
+void return_true_if_values_is_sorted_from_small_to_large(){
+    printf("\nsmall_to_large() should return true if values is sorted from small to large: ");
+    int values[10] = {1,2,3,4,5,6,7,8,9,10};
+    if (small_to_large(values, 10)) {
+        printf("SUCCESS\n");
+        return;
+    }
+    printf("FAIL\n");
+}
+void return_false_if_values_is_not_sorted_from_small_to_large(){
+    printf("\nsmall_to_large() should return true if values is not sorted from small to large: ");
+    int values[10] = {1,4,3,4,5,6,7,8,9,10};
+    if (small_to_large(values, 10)) {
+        printf("FAIL\n");
+        return;
+    }
+    printf("SUCCESS\n");
 }
