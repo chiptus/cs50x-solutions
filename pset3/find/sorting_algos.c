aa07dc476a9568d42c4ed33c1c2f0ab1ca1f4077
Chaim Lando <ubuntu@dfc2eabdf236>
2017-04-24 14:33:35 +0000

    bubble sort

--- pset3/find/sorting_algos.c
+++ pset3/find/sorting_algos.c
@@ -4,7 +4,7 @@
  * Implementations of sorting algorithms
  * 
  **/
-
+#include <stdbool.h>
 #include "sorting_algos.h"
 
 void replace(int values[], int i, int j);
@@ -72,7 +72,24 @@ void bubble_sort(int values[], int n)
     } while (swap_count != 0);
 }
 
+void order_next_element(int values[], int n, int i);
+
 void insertion_sort(int values[], int n)
 {
-    
+    //values[0..i-1] are sorted
+    for (int i=1; i<n; i++)
+    {
+        order_next_element(values, n, i);
+    }
+}
+
+void order_next_element(int values[], int n, int i)
+{
+    bool flag = values[i-1] > values[i];
+    //values[j..i] > values[swap_index]
+    for (int j=i-1; j>=0 && flag; j--)
+    {
+        replace(values, j, j+1);
+        flag = values[j-1] > values[j]; 
+    }
 }
