f540feceeaf56a9124e5a219c569d51ffa4c6573
Chaim Lando <ubuntu@dfc2eabdf236>
2017-05-03 11:43:33 +0000

    init game of fifteen

---
+++ pset3/fifteen/fifteen.t.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() //int argc, string argv[]
+{
+    printf("WELCOME TO GAME OF FIFTEEN\n\n");
+    printf("8 7 6\n\n");
+    printf("5 4 3\n\n");
+    printf("2 1 _\n\n");
+    printf("Tilt to move:\n");
+}
