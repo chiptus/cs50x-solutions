477cb649342ecf97f32a7d4974649d6f2da02cdd
Chaim Lando <chiptus@gmail.com>
2017-10-18 12:48:35 +0000

    recognize jpg blocks

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -7,6 +7,8 @@ typedef uint8_t BYTE;
 const int BLOCK_SIZE = sizeof(BYTE) * 512;
 
 void recover(FILE *image);
+int is_jpeg(BYTE* block);
+int is_forth_byte(BYTE byte);
 
 int main(int argc, char *argv[])
 {
@@ -38,15 +40,28 @@ void recover(FILE *image)
     while (feof(image) == 0)
     {
         BYTE* block = malloc(BLOCK_SIZE);
-        fread(&block, BLOCK_SIZE, 1, image);
-        // if (is_jpeg(block))
-        // {
-        //     //start a new file
-        // }
-        // else
-        // {
-        //   // add to old file
-        // }
+        fread(block, BLOCK_SIZE, 1, image);
+        // printf("%ld\n",ftell(image));
+        if (is_jpeg(block))
+        {
+           printf("block is jpg\n");
+        }
+        else
+        {
+        //   printf("block isn't jpg");
+        }
         free(block);
     }
+}
+
+int is_jpeg(BYTE* block)
+{
+    return block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && is_forth_byte(block[3]);
+}
+
+int is_forth_byte(BYTE byte)
+{
+   return byte == 0xe0 || byte == 0xe1 || byte == 0xe2 || byte == 0xe3 || byte == 0xe4
+    || byte ==  0xe5 || byte == 0xe6 || byte == 0xe7 || byte == 0xe8 || byte == 0xe9
+     || byte == 0xea || byte == 0xeb || byte == 0xec || byte == 0xed || byte == 0xee || byte == 0xef;
 }
