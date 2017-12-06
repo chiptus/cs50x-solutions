2411691edbc5b073f1081db22b79a58014724d19
Chaim Lando <chiptus@gmail.com>
2017-12-06 16:39:18 +0000

    added trie interface

---
+++ pset5/speller/trie.h
@@ -0,0 +1,21 @@
+
+
+typedef struct trie_node 
+{
+    char value;
+    struct trie_node* next;
+    int end;
+} trie_node;
+
+typedef struct trie 
+{
+    trie_node head;
+} trie;
+
+trie init_trie();
+
+int is_word_in_trie(const trie trie, char* word);
+
+void insert_word(trie trie, char* word);
+
+
