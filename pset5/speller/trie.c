164be39572d4279491c1bb9894f73c7e029b904f
Chaim Lando <chiptus@gmail.com>
2017-12-14 18:29:00 +0000

    add word and check for word

--- pset5/speller/trie.c
+++ pset5/speller/trie.c
@@ -1,21 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include "trie.h"
 
+int get_char_index(char c) {
+    int i = (int)c;
+    if (isupper(c)) {
+        return i-65;
+    }
+    if (islower(c)) {
+        return i-97;
+    }
+    // c is '
+    return 26;
+}
 
+int is_char_valid(char c) {
+    return isalpha(c) || c == '\'';
+}
 
 int main() {
-    trie trie;
-    int answer = is_word_in_trie(&trie, "walla");
-    printf("trie should be empty %d\n", answer == 0);
+    trie* trie = malloc(sizeof(struct trie)) ;
+    int answer = is_word_in_trie(trie, "walla");
+    printf("trie should be empty: %s\n", answer == 0 ? "suc" : "fail");
+    insert_word(trie, "newword");
+    int new_word_in = is_word_in_trie(trie, "newword");
+    printf("trie should have newword: %s\n", new_word_in == 1 ? "suc" : "fail");
     return 0;
 }
 
 
 int is_word_in_trie(const trie* trie, char* word) {
+    if (trie->head == NULL) {
+        return 0;
+    }
+    trie_node* current = trie->head[get_char_index(word[0])];
+    if (current == NULL) {
+        return 0;
+    }
+    int i = 1;
+    int len = strlen(word);
+    // walk on the trie nodes following the word characters
+    while (i < len && current != NULL && current->next != NULL) {
+        current = current->next[get_char_index(word[i])];
+        i++;
+    }
+    if (i == len && current != NULL) {
+        return current->end;
+    }
     return 0;
 }
 
 void insert_word(trie* trie, char* word) {
-    
+    if (trie->head == NULL) {
+        trie->head = malloc(sizeof(trie_node) * 27);
+    }
+    int len = strlen(word);
+    if (trie->head[get_char_index(word[0])] == NULL) {
+        trie->head[get_char_index(word[0])] = malloc(sizeof(trie_node));
+    }
+    trie_node* current_node = trie->head[get_char_index(word[0])];
+    for (int i = 1; i < len; i++) {
+        int char_index = get_char_index(word[i]);
+        if (current_node->next == NULL) {
+            current_node->next = malloc(sizeof(trie_node) * 27);
+        }
+        if (current_node->next[char_index] == NULL) {
+            current_node->next[char_index] = malloc(sizeof(trie_node));
+        }
+        current_node = current_node->next[char_index];
+    }
+    current_node->end = 1;
 }
--- pset5/speller/trie.h
+++ pset5/speller/trie.h
@@ -2,18 +2,17 @@
 
 typedef struct trie_node 
 {
-    char value;
-    struct trie_node* next;
+    struct trie_node** next;
     int end;
 } trie_node;
 
 typedef struct trie 
 {
-    trie_node* head;
+    trie_node** head;
 } trie;
 
 int is_word_in_trie(const trie* trie, char* word);
 
 void insert_word(trie* trie, char* word);
 
-
+void unload_trie(trie* trie);
