8de3ae5edbebd5022e6095319e60071dde48a610
Chaim Lando <chiptus@gmail.com>
2018-02-13 06:07:16 +0000

    speller protect redefine .h

--- pset5/speller/hash-table.h
+++ pset5/speller/hash-table.h
@@ -1,11 +1,14 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
 #include <stdbool.h>
 
 #include "linked-list.h"
 
-const int HASH_TABLE_SIZE = 10^5;
+static const int HASH_TABLE_SIZE = 10^5;
 
 typedef struct hash_table {
-    list slots[HASH_TABLE_SIZE];
+    list* buckets[HASH_TABLE_SIZE];
     int count;
 } hash_table;
 
@@ -16,3 +19,5 @@ bool is_word_in_table(const hash_table* t, const char* word);
 void insert_word(hash_table* t, char* word, int len);
 
 bool unload_table(hash_table* t);
+
+#endif
