8f63b6488bfdaa67195804a09dc734dbdc3f8e79
Chaim Lando <chiptus@gmail.com>
2018-02-13 06:06:34 +0000

    speller - test files

--- pset5/speller/test_hash_table.c
+++ pset5/speller/test_hash_table.c
@@ -5,12 +5,13 @@ int main() {
     //emtpy list
     hash_table* t = init_table();
     //check if empty
-    printf("should be false: %i", is_word_in_table(t, "hello"));
-    printf("should be zero: %i", t->count);
+    printf("should be false: %i\n", is_word_in_table(t, "hello"));
+    printf("should be zero: %i\n", t->count);
     //insert word
     insert_word(t, "hello", 5);
-    printf("should be true: %i", is_word_in_table(t, "hello"));
-    printf("should be one: %i", t->count);
+    printf("should be true: %i\n", is_word_in_table(t, "hello"));
+    printf("should be one: %i\n", t->count);
     
-    unload_table(t);
+    printf("should be true: %i\n", unload_table(t));
+    
 }
--- pset5/speller/test_linked_list.c
+++ pset5/speller/test_linked_list.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "linked-list.h"
 
 int main() {
@@ -6,6 +7,13 @@ int main() {
     l->head = init_node("hello");
     print_list(l);
     insert_to_top(l, "chamana");
+    node* n = find_in_list(l, "chamana");
+    printf("shouldn't be null: %i\n", n != NULL);
+    
+    node* not_found_node = find_in_list(l, "nothere");
+    printf("should be null: %i\n", not_found_node == NULL);
+    
+    
     print_list(l);
     unload_list(l);
 }
