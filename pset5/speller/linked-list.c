2b05d4cb65c7cc8d03e2be0a9b38e7880a1b0b18
Chaim Lando <chiptus@gmail.com>
2018-03-05 10:48:23 +0000

    add comments to linked-list.c

--- pset5/speller/linked-list.c
+++ pset5/speller/linked-list.c
@@ -4,47 +4,63 @@
 
 #include "linked-list.h"
 
-list* init_list() {
+// Initializes a new list
+list* init_list() 
+{
     return (list *)(calloc(1, sizeof(list)));
 }
 
-node* init_node(const char* value) {
-    node * n = (node *)(calloc(1, sizeof(node)));;
-    n->value= (char *)value;
+// Init a new node
+node* init_node(const char *value) 
+{
+    node *n = (node *)(calloc(1, sizeof(node)));;
+    n->value = (char *)value;
     return n;
 }
 
-node* insert(node* n, const char* value) {
-    node* new_node = init_node(value);
+// Creates a new node that points to the provided node
+node* insert(node* n, const char* value) 
+{
+    node *new_node = init_node(value);
     new_node->next = n;
     return new_node;
 }
 
-void insert_to_top(list* l, const char* value) {
+// Insert a node to the top of the list
+void insert_to_top(list *l, const char *value) 
+{
     l->head = insert(l->head, value);
     l->count++;
 }
 
-node* unload_node(node* n) {
-    node* next = n->next;
+// Unloads the node and returns the next node
+node* unload_node(node* n) 
+{
+    node *next = n->next;
     n->next = NULL;
     free(n->value);
     free(n);
     return next;
 }
 
-void unload_list(list* l) {
-    node* current = l->head;
-    while (current != NULL) {
+// Unloads the list
+void unload_list(list *l) 
+{
+    node *current = l->head;
+    while (current != NULL) 
+    {
         current = unload_node(current);
     }
     free(l);
 }
  
-node* find_in_list(list* l, const char* value) {
-    node* current = l->head;
+// Finds the first node in the list that contains the value
+node* find_in_list(list* l, const char *value) 
+{
+    node *current = l->head;
     while (current != NULL) {
-        if (strcmp(current->value, value) == 0) {
+        if (strcmp(current->value, value) == 0) 
+        {
             return current;
         }
         current = current->next;
