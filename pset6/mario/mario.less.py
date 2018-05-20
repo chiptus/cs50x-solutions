3becdd2beb5151030be6bafa17488c1c38763e78
Chaim Lando <>
2018-05-20 09:19:48 +0000

    pset6 - mario.less vFinal

--- pset6/mario/mario.less.py
+++ pset6/mario/mario.less.py
@@ -1,13 +1,36 @@
 from cs50 import get_int
 
+
 def main():
-    print_pyramid(get_int("Height:"))
-    
+    """
+    Main Method
+    """
+    print_pyramid(get_height())
+
+
 def print_pyramid(height):
+    """
+    Prints the pyramid with `height`
+    """
     for i in range(height):
         print(create_line(i, height))
-        
+
+
 def create_line(line, height):
+    """
+    Prints one line of the pyramid
+    """
     return " " * (height - line) + "#" * (line + 1)
+
+
+def get_height():
+    """
+    Gets `height` input from the user
+    """
+    while True:
+        val = get_int("Height: ")
+        if 0 <= val <= 23:
+            return val
+
 
 main()
