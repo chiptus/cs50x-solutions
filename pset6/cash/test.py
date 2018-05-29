45f64a7af8acebc816127207542cdbf839a3a3f5
Chaim Lando <>
2018-05-29 03:54:35 +0000

    pset6 - cash - reformat + comments

--- pset6/cash/test.py
+++ pset6/cash/test.py
@@ -8,14 +8,11 @@ class TestCalcAmountOfCoins(unittest.TestCase):
     def test_input_of_0_41_yields_output_of_4(self):
         self.assertEqual(cash.calc_amount_of_coins(0.41), 4)
 
-
     def test_input_of_0_01_yields_output_of_1(self):
         self.assertEqual(cash.calc_amount_of_coins(0.01), 1)
 
-    
     def test_input_of_0_15_yields_output_of_2(self):
         self.assertEqual(cash.calc_amount_of_coins(0.15), 2)
-
 
     def test_input_of_1_6_yields_output_of_7(self):
         self.assertEqual(cash.calc_amount_of_coins(1.6), 7)
@@ -27,5 +24,5 @@ class TestCalcAmountOfCoins(unittest.TestCase):
         self.assertEqual(cash.calc_amount_of_coins(4.2), 18)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
